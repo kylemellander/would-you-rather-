83469e15d4cc310329f5cc3a4377beba9b017e68
Brian Lau & Kyle Mellander <student@epicodus.com>
2015-10-26 16:45:36 -0700

    Add voting

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,19 @@
+class VotesController < ApplicationController
+  def create
+    @choice = Choice.find(params[:choice_id].to_i)
+    @question = Question.find(params[:question_id].to_i)
+    @vote = Vote.new(choice_id: @choice.id)
+    if @vote.save
+      respond_to do |format|
+        format.html { redirect_to root_path, notice: 'Nice Vote.' }
+        format.js
+      end
+    end
+  end
+
+  private
+
+  def vote_params
+    params.require(:choice_id)
+  end
+end
