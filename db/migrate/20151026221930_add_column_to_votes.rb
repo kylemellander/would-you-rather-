3bd937b6a903e36e4695e9d34c6520d05cac1cdc
Brian Lau & Kyle Mellander <student@epicodus.com>
2015-10-26 15:40:01 -0700

    Initialize model for votes

---
+++ db/migrate/20151026221930_add_column_to_votes.rb
@@ -0,0 +1,5 @@
+class AddColumnToVotes < ActiveRecord::Migration
+  def change
+    add_column :votes, :value, :integer, default: 1
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026163428) do
+ActiveRecord::Schema.define(version: 20151026221930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20151026163428) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "choice_id"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "value",      default: 1
   end
 
 end
